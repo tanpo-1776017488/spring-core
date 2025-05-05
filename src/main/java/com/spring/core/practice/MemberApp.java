3be8626bac788518427d1f4ad86d0b2dba8fce61
Jaehyeon Hwnag <tanpo1733@naver.com>
2025-05-05 15:44:59 +0900

    feat: Add member join service
    Description:
    Client uses MemberService
    MemberService uses MemberRepository
    MemoryRepository is an implementation of MemberRepository

---
+++ src/main/java/com/spring/core/practice/MemberApp.java
@@ -0,0 +1,18 @@
+package com.spring.core.practice;
+
+import com.spring.core.practice.member.Grade;
+import com.spring.core.practice.member.Member;
+import com.spring.core.practice.member.MemberService;
+import com.spring.core.practice.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService service = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        service.join(memberA);
+
+        Member findMember = service.findMember(1L);
+        System.out.println("new member = " + memberA.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
---
+++ src/main/java/com/spring/core/practice/member/Grade.java
@@ -0,0 +1,6 @@
+package com.spring.core.practice.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
