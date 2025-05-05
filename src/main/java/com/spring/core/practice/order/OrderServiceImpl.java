3d16e87611991640c4ebc1edf04a0a84bfe7e500
Jaehyeon Hwnag <tanpo1733@naver.com>
2025-05-05 17:06:38 +0900

    feat: change discount policy
    Description:
    change discount policy for CR

--- src/main/java/com/spring/core/practice/order/OrderServiceImpl.java
+++ src/main/java/com/spring/core/practice/order/OrderServiceImpl.java
@@ -2,13 +2,14 @@ package com.spring.core.practice.order;
 
 import com.spring.core.practice.discount.DiscountPolicy;
 import com.spring.core.practice.discount.FixedDiscountPolicy;
+import com.spring.core.practice.discount.RateDiscountPolicy;
 import com.spring.core.practice.member.Member;
 import com.spring.core.practice.member.MemberRepository;
 import com.spring.core.practice.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
