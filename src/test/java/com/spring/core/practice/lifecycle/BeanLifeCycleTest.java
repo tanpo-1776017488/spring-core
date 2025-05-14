d3f23bd4bf8e880b40eb088826519d1f7f04b390
Jaehyeon Hwnag <tanpo1733@naver.com>
2025-05-15 00:12:20 +0900

    test: Add example of bean's lifecycle
    Description:
    After setting the dependencies, InitializingBean::afterPropertiesSet is called.
    Before the bean is destroyed, DisposableBean::destroy() is called.
    
    The above example has a strong dependency on Spring and cannot be applied to external libraries, which is why it is not widely used anymore.

---
+++ src/test/java/com/spring/core/practice/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package com.spring.core.practice.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); // for life cycle test
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
---
+++ src/test/java/com/spring/core/practice/lifecycle/NetworkClient.java
@@ -0,0 +1,41 @@
+package com.spring.core.practice.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자를 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect(){
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + " message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
