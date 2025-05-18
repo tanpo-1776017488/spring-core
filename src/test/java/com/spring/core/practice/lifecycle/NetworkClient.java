1754527cba9f77582eac414d616a4ba0b556707e
Jaehyeon Hwnag <tanpo1733@naver.com>
2025-05-18 16:31:27 +0900

    feat: Add example of bean's lifecycle
    Description:
    Because it follows the Java standard, it can also run in containers of frameworks other than Spring.

--- src/test/java/com/spring/core/practice/lifecycle/NetworkClient.java
+++ src/test/java/com/spring/core/practice/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package com.spring.core.practice.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -23,13 +26,15 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init()  {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
 
-    public void close() throws Exception {
+    @PreDestroy
+    public void close()  {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
